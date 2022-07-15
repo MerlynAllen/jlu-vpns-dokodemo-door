77d9b33fbd9c929ec8b38b4c811398a03ea82ee7
Merlyn Allen <me@xiaorui.li>
2022-07-16 00:36:01 +0800

    Added heroku.

--- jump.js
+++ jump.js
@@ -4,23 +4,23 @@ var key = 'wrdvpnisthebest!';
 var iv = 'wrdvpnisthebest!';
 
 function encrypt_url(base_url) {
-    console.log(base_url);
-    var cipher = crypto.createCipheriv('aes-128-cfb', key, iv);
     const fullurl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
     const partialurl = /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
-    if (base_url.match(fullurl)) { // if url contains protocol
-        var protocol = base_url.split('://')[0];
-        var host = base_url.split('://')[1].split('/')[0];
-        var path = base_url.split('://')[1].split('/')[1];
+
+    console.log(base_url);
+    if (base_url.match(fullurl)) {
+        base_url = new URL(base_url);
     } else if (base_url.match(partialurl)) {
-        var protocol = "http" // default protocol
-        var host = base_url.split('://')[1].split('/')[0];
-        var path = base_url.split('://')[1].split('/')[1];
+        base_url = new URL('http://' + base_url);
     } else {
         return "about:blank";
     }
+    var cipher = crypto.createCipheriv('aes-128-cfb', key, iv);
+    var protocol = base_url.protocol.split(':')[0];
+    var host = base_url.host;
+    var path = base_url.pathname + base_url.search;
     var enctypted_host = cipher.update(host, 'utf8', 'hex');
-    var url = "https://webvpn.jlu.edu.cn/" + protocol + "/77726476706e69737468656265737421" + enctypted_host + "/" + path;
+    var url = "https://webvpn.jlu.edu.cn/" + protocol + "/77726476706e69737468656265737421" + enctypted_host + path;
     console.log(url);
     return url;
 }
@@ -35,4 +35,4 @@ var handler = function(request, response) {
 }
 var server = http.createServer(handler);
 
-server.listen(process.env.PORT);
+server.listen(process.env.PORT || 8080);
