90013c93acb714d58d603fe16e9c35e7c6ccb818
Merlyn <merlyn@Merlyns-MacBook-Air.local>
2023-02-26 14:06:20 +0800

    移除了在线部署服务，修改为本地部署版本。增加了主页以便浏览器记录页面历史，方便后续使用。移除了旧版纯302的方式。重写了README以适应功能改变。

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # JLU VPNS Dokodemo Door
-一个简单的Node.js后端部署在Heroku上。只负责URL转换并302到新URL。
-跳转url只需要 `http://webvpn.jlu.merlyn.cc/`+`<原URL>`即可。如果原URL不合法会自动跳转到`about:blank`
----
-**以下为旧版**
-其实就是一个重定向任意门，打开[这个页面](https://webvpn.jlu.edu.cn/https/77726476706e69737468656265737421fdf253903e3e695c720d87e29f5c373d3f27c82a2a/jlu-vpns-dokodemo-door/)，再使用重定向功能就可以用打开JLU vpns代理的网页。
-更新：
-1. 新增url传参方式直接跳转，url后拼接"?"+目标url即可。
-2. 由于WebVPN改版，更新一下url。
+Node.js脚本，用于将任意（中国大陆可以访问的）URL转换并跳转到JLU Web VPN的URL。
+## Prerequisite
+需要[npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm/)和[Node.js](https://nodejs.org/en/)
+## Usage
+- 
+    ```shell
+    npm run start
+    ```  
+    之后启动浏览器访问<http://localhost:8080>可。  
+- 也支持直接在主机名后直接拼接目标网址的URL。如<http://localhost:8080/https://www.baidu.com/>  
+- 可能会跳转到登陆界面，完成登陆后重新尝试一次即可。
--- jump.js
+++ jump.js
@@ -5,6 +5,35 @@ const http = require('http');
 var key = 'jluvpnisthegood!';
 var iv = 'jluvpnisthegood!';
 
+function main_page() {
+    return `
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>JLU VPN 任意门</title>
+    </head>
+    <body>
+    <h1>JLU VPN 任意门</h1>
+    <p>JLU VPN 任意门是一个可以使用JLU Web VPN带你跳转到任意目标页面的工具。</p>
+    <input type="text" id="url" placeholder="URL" >
+    <button onclick="jump()">Jump</button>
+    <p>常用站点:
+    <ul>
+    <li><a href="https://vpn.jlu.edu.cn/https/6a6c7576706e6973746865676f6f642146ab1ccaa88ac2a733d52e8cff/">百度</a></li>
+    <li><a href="https://vpn.jlu.edu.cn/https/6a6c7576706e6973746865676f6f642152b24586a385cced259420/">必应</a></li>
+    </ul>
+    </p>
+    </body>
+    <script>
+    function jump() {
+        var url = document.getElementById("url").value;
+        window.location.href = window.location.href + url;
+    }
+    </script>
+    </html>
+    `;
+}
+
 
 function encrypt_url(base_url) {
     const fullurl = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
@@ -30,6 +59,13 @@ function encrypt_url(base_url) {
 
 var handler = function(request, response) {
     var url = request.url.substr(1);
+    if (url == "") {
+        response.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        response.end(main_page());
+        return;
+    }
     var encrypted_url = encrypt_url(url);
     response.writeHead(302, {
         'Location': encrypted_url
@@ -39,3 +75,4 @@ var handler = function(request, response) {
 var server = http.createServer(handler);
 
 server.listen(process.env.PORT || 8080);
+console.log("Server started at http://localhost:8080/");
